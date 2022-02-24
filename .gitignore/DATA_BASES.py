a07dc4c45e8f1a027e0260abab3244b1ab0dc0b5
hwangstone1 <mylee553@naver.com>
2022-02-24 21:27:48 +0900

    exercise_project

---
+++ .gitignore/DATA_BASES.py
@@ -0,0 +1,12 @@
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'exercise',
+        'USER': 'root',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
+SECRET_KEY = 'django-insecure-9l#t#1h6#mudw*urm91)&opi-4k&ae_+%%)co^pqg7geiq=9%q'
