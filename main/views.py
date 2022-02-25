d90174bdc74e04a0c496a6fc224dbd2eac199a95
hwangstone1 <mylee553@naver.com>
2022-02-25 16:28:51 +0900

    -

--- main/views.py
+++ main/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import Question, Answer
 from django.utils import timezone
-# Create your views here.
+
+from .forms import QuestionForm, AnswerForm
+from .models import Question
+
 
 def main(request):
 
@@ -10,6 +12,7 @@ def main(request):
     context = {'question_list': question_list}
     return render(request, 'main/question_list.html', context)
 
+
 def detail(request, question_id):
 
 
@@ -17,9 +20,35 @@ def detail(request, question_id):
     context = {'question': question}
     return render(request, 'main/question_detail.html', context)
 
+
 def answer_create(request, question_id):
 
 
     question = get_object_or_404(Question, pk=question_id)
-    question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
-    return redirect('main:detail', question_id=question.id)
+    if request.method == "POST":
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.create_date = timezone.now()
+            answer.question = question
+            answer.save()
+            return redirect('main:detail', question_id=question.id)
+    else:
+        form = AnswerForm()
+    context = {'question': question, 'form': form}
+    return render(request, 'main/question_detail.html', context)
+
+
+def question_create(request):
+
+    if request.method == 'POST':
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.create_date = timezone.now()
+            question.save()
+            return redirect('main:main')
+    else:
+        form = QuestionForm()
+    context = {'form': form}
+    return render(request, 'main/question_form.html', context)
