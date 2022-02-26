52f2e7e46c64b427eed05e8cb1a260a64769ba64
hwangstone1 <mylee553@naver.com>
2022-02-26 17:42:11 +0900

    -

--- main/views.py
+++ main/views.py
@@ -1,6 +1,7 @@
+from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
-from django.core.paginator import Paginator
 
 from .forms import QuestionForm, AnswerForm
 from .models import Question
@@ -8,24 +9,29 @@ from .models import Question
 
 def main(request):
 
-    page = request.GET.get('page', 1)
+
+    # 입력 파라미터
+    page = request.GET.get('page', '1')  # 페이지
+
+    # 조회
     question_list = Question.objects.order_by('-create_date')
 
-    #페이징처리
-    paginator = Paginator(question_list, 10)
+    # 페이징처리
+    paginator = Paginator(question_list, 10)  # 페이지당 10개씩 보여주기
     page_obj = paginator.get_page(page)
+
     context = {'question_list': page_obj}
     return render(request, 'main/question_list.html', context)
 
 
 def detail(request, question_id):
 
-
     question = get_object_or_404(Question, pk=question_id)
     context = {'question': question}
     return render(request, 'main/question_detail.html', context)
 
 
+@login_required(login_url='common:login')
 def answer_create(request, question_id):
 
 
@@ -34,6 +40,7 @@ def answer_create(request, question_id):
         form = AnswerForm(request.POST)
         if form.is_valid():
             answer = form.save(commit=False)
+            answer.author = request.user  # 추가한 속성 author 적용
             answer.create_date = timezone.now()
             answer.question = question
             answer.save()
@@ -44,12 +51,15 @@ def answer_create(request, question_id):
     return render(request, 'main/question_detail.html', context)
 
 
+@login_required(login_url='common:login')
 def question_create(request):
+
 
     if request.method == 'POST':
         form = QuestionForm(request.POST)
         if form.is_valid():
             question = form.save(commit=False)
+            question.author = request.user  # 추가한 속성 author 적용
             question.create_date = timezone.now()
             question.save()
             return redirect('main:main')
